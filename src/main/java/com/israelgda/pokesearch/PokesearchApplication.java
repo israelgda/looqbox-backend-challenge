18665bc52bab4d17035153bd3116ad9ee49bde4f
Israel Gadelha <gdelha.israel@gmail.com>
2022-01-22 23:25:05 -0300

    Commit inicial do projeto

---
+++ src/main/java/com/israelgda/pokesearch/PokesearchApplication.java
@@ -0,0 +1,13 @@
+package com.israelgda.pokesearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokesearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokesearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/israelgda/pokesearch/PokesearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.israelgda.pokesearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokesearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
